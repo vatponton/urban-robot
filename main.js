f0aff22b9540b48c081488446c37037cd3a3413a
Ferdie Hal <165138090+vatponton@users.noreply.github.com>
2024-03-27 09:49:33 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+  var variationsWithRepetition = (function () {
+    var res;
+
+    function variations(arr, k, index, current) {
+      if (k === index) {
+        return res.push(current.slice());
+      }
+      for (var i = 0; i < arr.length; i += 1) {
+        current[index] = arr[i];
+        variations(arr, k, index + 1, current);
+      }
+    }
+
+
+                                    return function (arr, k) {
+      res = [];
+      variations(arr, k, 0, []);
+      var temp = res;
+      res = undefined;
+      return temp;
+    };
+  }());
+
+  exports.variationsWithRepetition = variationsWithRepetition;
+
+}((typeof window === 'undefined') ? module.exports : window));
